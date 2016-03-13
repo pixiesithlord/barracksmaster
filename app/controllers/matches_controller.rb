f6511ee43fd9ab208d2559cc2f01417e648ba933
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-03-13 18:00:00 +0800

    Add models and api calls

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,9 +1,26 @@
 class MatchesController < ApplicationController
   def index
+    @matches = Match.all.limit(20)
   end
 
   def create
-    puts params if params
-    redirect_to root_path
+    payload  = JSON.parse(params['payload'])
+    players  = payload['rounds']['0']['players']
+    match_id = payload["matchID"]
+
+    unless Match.find_by(match_id: match_id)
+      m = Match.create(
+        payload:    payload,
+        match_id:   match_id,
+        player_ids: players.map { |p| p['steamID32']}
+      )
+    end
+
+    players.each do |player_data|
+      p           = Player.find_or_create_by(steamID: player_data["steamID32"])
+      p.points    = p.points.to_i + player_data['pt']
+    end
+
+    render nothing: true
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,13 @@
 class Match < ActiveRecord::Base
+  # Payload
+  # lm = lumber, 
+  # nw = net worth, 
+  # gl = gold, 
+  # cs = creep score/creep kills, 
+  # pt = no. of points earned (this is the key one), 
+  # ph = i think this is the hero? should be la huh, 
+  # steam id = the steam no. to identify the exact player 
+  # matchID = the identifying no. for that match played
+  
+  
 end
