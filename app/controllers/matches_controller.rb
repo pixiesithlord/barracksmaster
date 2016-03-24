6ba891ca465f942b329d3cd22868c9f383efbd67
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-03-24 22:46:22 +0800

    save just payload

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,22 +5,22 @@ class MatchesController < ApplicationController
 
   def create
     payload  = JSON.parse(params['payload'])
-    players  = payload['rounds']['0']['players']
-    match_id = payload["matchID"]
+    # players  = payload['rounds']['0']['players']
+    match_id = payload["dotaMatchID"]
 
     unless Match.find_by(match_id: match_id)
       m = Match.create(
         payload:    payload,
-        match_id:   match_id,
-        game_time:  payload['rounds']['0']['game']['dr']/60,
-        player_ids: players.map { |p| p['steamID32']}
+        # match_id:   match_id,
+        # game_time:  payload['rounds']['0']['game']['dr']/60,
+        # player_ids: players.map { |p| p['steamID32']}
       )
     end
 
-    players.each do |player_data|
-      player = Player.find_or_create_by(steamID: player_data["steamID32"])
-      player.update(points: player.points.to_i + player_data['pt'].to_i)
-    end
+    # players.each do |player_data|
+    #   player = Player.find_or_create_by(steamID: player_data["steamID32"])
+    #   player.update(points: player.points.to_i + player_data['pt'].to_i)
+    # end
 
     render nothing: true
   end
