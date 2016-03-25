98242762873748cd2ed76ef922fa2cf71b4bf4af
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-03-25 17:31:17 +0800

    Setup player creation and payload mapping

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,20 +5,21 @@ class MatchesController < ApplicationController
 
   def create
     payload  = JSON.parse(params['payload'])
-    players  = payload['players']
 
     render nothing: true if players.count == 1
 
+    m = Match.new
+
     unless Match.find_by(match_id: match_id)
-      m = Match.new(payload: payload)
-      # m.map_payload
+      m.payload = payload
+      m.map_payload
       m.save
     end
 
-    # players.each do |player_data|
-    #   player = Player.find_or_create_by(steamID: player_data["steamID32"])
-    #   player.update(points: player.points.to_i + player_data['pt'].to_i)
-    # end
+    m.player_ids.each do |id|
+      player = Player.find_or_create_by(steamID: id)
+      player.update(points: player.points.to_i + player_data['pt'].to_i)
+    end
 
     render nothing: true
   end
