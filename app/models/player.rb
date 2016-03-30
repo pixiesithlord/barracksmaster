1cc7b4374bb98d4897b42bd0f82ca11e9201a42f
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-03-31 00:51:44 +0800

    change steam info that is set

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,7 +1,7 @@
 class Player < ActiveRecord::Base
   default_scope {order(points: :desc)}
   after_create :set_steam_info
-  
+
   def self.steamID_from_32_to_64(id)
     ('765' + (id + 61197960265728).to_s).to_i
   end
@@ -13,6 +13,6 @@ class Player < ActiveRecord::Base
   def set_steam_info
     uri  = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=96DD4E3D461672DDA2361EE82C51A7B5&steamids='
     resp = JSON.parse(Net::HTTP.get(URI(uri + steamID.to_s)))
-    self.update_attributes(steam_data: resp)
+    self.update_attributes(steam_data: resp['response']['players'][0])
   end
 end
