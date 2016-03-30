02642320e4f81f7b503a8f3097f53d2eaf1e9458
Sherwyn Goh <sherwyn.lj.goh@gmail.com>
2016-03-31 00:52:45 +0800

    Limit matches to latest 200

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,11 @@
 class MatchesController < ApplicationController
   def index
-    @matches = Match.all
+    @matches = Match.limit(200)
   end
 
   def create
     payload = JSON.parse(params['payload'])
-    
+
     render nothing: true if payload['players'].count == 1
 
     match_id = payload['dotaMatchID']
